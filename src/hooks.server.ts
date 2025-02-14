7c6060ceb090aec88e70b5bd47fb0a67f63faadb
Mary <git@mary.my.id>
2025-02-14 18:04:20 +0700

    feat: clarify if it's us, or upstream error

---
+++ src/hooks.server.ts
@@ -0,0 +1,20 @@
+import { XRPCError } from '@atcute/client';
+import type { HandleServerError } from '@sveltejs/kit';
+
+export const handleError: HandleServerError = async ({ error, event, status, message }) => {
+	console.error(error);
+
+	if (error instanceof XRPCError) {
+		switch (error.kind) {
+			case 'InternalServerError': {
+				return {
+					message: `Upstream server returned an internal server error`,
+				};
+			}
+		}
+	}
+
+	return {
+		message: `Something went wrong, sorry about that`,
+	};
+};
