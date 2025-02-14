ffda47b7c009416b8d712500f840a1f5d7fe3511
Mary <git@mary.my.id>
2025-02-14 18:06:11 +0700

    feat: throw proper errors for profiles

--- src/routes/(app)/(profile)/[actor=didOrHandle]/+layout.ts
+++ src/routes/(app)/(profile)/[actor=didOrHandle]/+layout.ts
@@ -1,4 +1,5 @@
-import { XRPC, simpleFetchHandler } from '@atcute/client';
+import { XRPC, XRPCError, simpleFetchHandler } from '@atcute/client';
+import { error } from '@sveltejs/kit';
 
 import { PUBLIC_APPVIEW_URL } from '$env/static/public';
 
@@ -7,13 +8,31 @@ import type { LayoutLoad } from './$types';
 export const load: LayoutLoad = async ({ params, fetch }) => {
 	const rpc = new XRPC({ handler: simpleFetchHandler({ service: PUBLIC_APPVIEW_URL }) });
 
-	const { data } = await rpc.get('app.bsky.actor.getProfile', {
-		params: {
-			actor: params.actor,
-		},
-	});
+	try {
+		const { data } = await rpc.get('app.bsky.actor.getProfile', {
+			params: {
+				actor: params.actor,
+			},
+		});
 
-	return {
-		profile: data,
-	};
+		return {
+			profile: data,
+		};
+	} catch (err) {
+		if (err instanceof XRPCError) {
+			switch (err.kind) {
+				case 'InvalidRequest': {
+					error(404, `Account doesn't exist`);
+				}
+				case 'AccountTakedown': {
+					error(404, `Account is taken down`);
+				}
+				case 'AccountDeactivated': {
+					error(404, `Account is deactivated`);
+				}
+			}
+		}
+
+		throw err;
+	}
 };
