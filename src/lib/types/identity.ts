9b8d50366902a6e8e866bbff1605bc324f41453c
Mary <git@mary.my.id>
2025-02-15 09:04:01 +0700

    feat: checks for atproto did

--- src/lib/types/identity.ts
+++ src/lib/types/identity.ts
@@ -15,3 +15,20 @@ export const DID_RE = /^(?=.{7,2048}$)did:([a-z]+):([a-zA-Z0-9._:%\-]*[a-zA-Z0-9
 export const isDid = (input: string): input is Did => {
 	return input.length >= 7 && input.length <= 2048 && DID_RE.test(input);
 };
+
+export const ATPROTO_WEB_DID_RE =
+	/^did:web:([a-zA-Z0-9\-]+(?:\.[a-zA-Z0-9\-]+)*(?:\.[a-zA-Z]{2,})|localhost(?:%3[aA]\d+)?)$/;
+
+export const isAtprotoWebDid = (input: string): input is Did<'web'> => {
+	return input.length >= 12 && ATPROTO_WEB_DID_RE.test(input);
+};
+
+export const PLC_DID_RE = /^did:plc:([a-z2-7]{24})$/;
+
+export const isPlcDid = (input: string): input is Did<'plc'> => {
+	return input.length === 32 && PLC_DID_RE.test(input);
+};
+
+export const isAtprotoDid = (input: string): input is AtprotoDid => {
+	return isPlcDid(input) || isAtprotoWebDid(input);
+};
